1ee1690c67a7a23bccf1d834d43d79460e100e6d
Ben Gao <59623966+bengao98@users.noreply.github.com>
2021-01-11 22:37:41 -0800

    Add files via upload

---
+++ rollbot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command == 'roll'){
+        const taggedUser = message.mentions.users.first();
+
+        message.channel.send(`${taggedUser.username} rolls ${Math.floor(Math.random() * 100) + 1} (1-100)`);
+    } 
+
+});
+
+client.login(config.token);
